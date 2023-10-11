6ee291dd4f2a95dc5839a112f33f901e01f0782c
CasePaper33831 <87284113+CasePaper33831@users.noreply.github.com>
2023-10-11 16:45:41 -0500

    Update code.py
    code update, TODO update

--- code.py
+++ code.py
@@ -1,29 +1,33 @@
 '''
-TODO:binary fill entire popup
+TODO:ummmmm wut happened to running the code? lets get that sorted
+
+FINISH:
+binary fill entire popup
 '''
 import tkinter as tk
 import random
 
 def the_fun_stuff():
     binary_str = ""
-    for i in range(10):
+    for i in range(10000):
         num = random.randint(0, 1)
         binary_str += str(num)
     return binary_str
 
 def show_popup():
-    popup = tk.Tk()
-    popup.title("look at me. Im the popup now")
-    popup.geometry("1000x1000")
-    output_text = tk.Text(popup, font=("Helvetica", 10))
-    output_text.pack(fill=tk.BOTH, expand=True)
-    
-    while True:
+    def update_text():
         binary_output = the_fun_stuff()
         output_text.delete(1.0, tk.END)
         output_text.insert(tk.END, binary_output)
-        popup.update_idletasks()
-        popup.update()
+        popup.after(500, update_text)
 
-if __name__ == "__main__":
-    show_popup()
+    popup = tk.Tk()
+    popup.title("Look at me. I'm the popup now")
+    popup.geometry("1000x1000")
+    output_text = tk.Text(popup, font=("Helvetica", 10), fg="green")
+    output_text.pack(fill=tk.BOTH, expand=True)
+    popup.configure(bg="black")
+    
+    update_text()
+
+    popup.mainloop()
